da3791406da4f187cd65b3d85225aacfe7df4c7b
Chris Pyles <cpyles@berkeley.edu>
2022-02-13 18:40:06 -0800

    added timeout

--- run_pybryt.py
+++ run_pybryt.py
@@ -1,3 +1,5 @@
+"""A command-line program for running PyBryt on GitHub Actions"""
+
 import argparse
 import dill
 import os
@@ -12,25 +14,80 @@ PARSER = argparse.ArgumentParser()
 PARSER.add_argument("--additional-files")
 PARSER.add_argument("--references")
 PARSER.add_argument("--subm")
+PARSER.add_argument("--timeout")
 
 
-def parse_list_arg(ref_urls):
-    return [u.strip() for u in ref_urls.split("\n") if u.strip()]
+is_none = lambda s: s.lower() == "none"
+"""a function to determine if a user-supplied value is None"""
+
+
+def parse_list_arg(args):
+    """
+    Parse a list of newline-delimited arguments, returning a list of strings with leading and
+    trailing whitespace stripped.
+
+    Parameters
+    ----------
+    args : str
+        the arguments
+
+    Returns
+    -------
+    list[str]
+        the parsed arguments
+    """
+    return [u.strip() for u in args.split("\n") if u.strip()]
 
 
 def get_full_path(repo_path):
+    """
+    Get the absolute path of a file in the repo using the GITHUB_WORKSPACE environment variable.
+
+    Parameters
+    ----------
+    repo_path : str
+        the relative path to a file from the root of the repo
+
+    Returns
+    -------
+    str
+        the file's absolute path
+    """
     workspace_path = os.environ["GITHUB_WORKSPACE"]
     return os.path.join(workspace_path, repo_path)
 
 
 def download_url(url, save_path):
+    """
+    Visit a URL and save the response's body to a file.
+
+    Parameters
+    ----------
+    url       : str
+        the URL to visit
+    save_path : str
+        the path at which to save the response
+    """
     url = urllib.request.urlopen(url)
     with open(save_path, "wb") as f:
         f.write(url.read())
 
 
 def main():
+    """
+    Run PyBryt using the arguments provided in ``sys.argv``.
+    """
     args = PARSER.parse_args()
+
+    # parse the timeout
+    if not args.timeout:
+        timeout = 1200
+    elif is_none(args.timeout):
+        timeout = None
+    elif args.timeout.isdigit():
+        timeout = int(args.timeout)
+    else:
+        raise ValueError("timeout must be a positive integer, 'none', or unspecified")
 
     ref_paths_or_urls = parse_list_arg(args.references)
     addl_filenames = [os.path.abspath(f) for f in parse_list_arg(args.additional_files)]
@@ -56,7 +113,7 @@ def main():
     subm_path = get_full_path(args.subm)
     print("Grading ", subm_path)
 
-    stu = pybryt.StudentImplementation(subm_path, addl_filenames=addl_filenames)
+    stu = pybryt.StudentImplementation(subm_path, addl_filenames=addl_filenames, timeout=timeout)
     res = stu.check(refs)
     report = pybryt.generate_report(res)
     print(report)
